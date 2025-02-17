b2d4848a6db02c96fe7eed9ecdd28993ae816b47
GGisMee <gustav.gamstedt@outlook.com>
2025-02-17 21:42:21 +0100

    added d_near to display

--- display_functionality/d2/app.py
+++ display_functionality/d2/app.py
@@ -4,43 +4,50 @@ import time
 import threading
 import random
 
-app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Shared data that updates in real-time
-current_data = {
-    "time": "00:00",
-    "d_front": 150,
-    "num_now": 3,
-    "antal_totalt": 10,
-    "extra": ""
-}
+class WebManager:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.socketio = SocketIO(self.app, cors_allowed_origins="*")
 
-def get_data():
-    """ Return the latest data """
-    return current_data
+        # Registrera route
+        self.app.route("/")(self.index)
 
-def update_data(time:str, d_front:float, num_now:int, warning_status: int):
-    """ Simulate real-time updates """
+        # Shared data that updates in real-time
+        self.current_data = {
+            "time": "00:00",
+            "d_front": 150,
+            "num_now": 3,
+            "status": 0,
+            "antal_totalt": 10,
+            "d_near": 150,
+        }
 
-    current_data["time"] = time
-    current_data["d_front"] = d_front  # Simulate sensor data
-    current_data["num_now"] = num_now
-    current_data["status"] = warning_status
-    current_data["antal_totalt"] = random.randint(20, 35)
-        
-    # Send updated data to all connected clients
-    socketio.emit('update', current_data)
-    time.sleep(1)  # Update every second
+    def get_data(self):
+        """ Return the latest data """
+        return self.current_data
 
-@app.route("/")
-def index():
-    return render_template("index.html", data=get_data())
+    def update_data(self, time:str='00:00', d_front:float='-', num_now:int='-', warning_status: int=0, d_near:int='-'):
+        """ Simulate real-time updates """
+
+        self.current_data["time"] = time
+        self.current_data["d_front"] = d_front
+        self.current_data["num_now"] = num_now
+        self.current_data["status"] = warning_status
+        self.current_data["d_near"] = d_near
+        # Skicka uppdaterade data till alla klienter
+        self.socketio.emit('update', self.current_data)
+        time.sleep(1)  # Uppdatera varje sekund
+
+    def index(self):
+        return render_template("index.html", data=self.get_data())
+
 
 if __name__ == "__main__":
-    # Start the update function in a background thread
-    thread = threading.Thread(target=update_data)
-    thread.daemon = True
+    webmanager = WebManager()
+
+    # Starta update_data() i en bakgrundstråd
+    thread = threading.Thread(target=webmanager.update_data, daemon=True)
     thread.start()
 
-    socketio.run(app, debug=True)
+    webmanager.socketio.run(webmanager.app, debug=True)
