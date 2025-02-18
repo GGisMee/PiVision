8e8ddae130517a23b0b8f7b0f5a1fb5aacfb3365
GGisMee <gustav.gamstedt@outlook.com>
2025-02-18 14:07:50 +0100

    Recovered lost files

--- camera_web/app.py
+++ camera_web/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
-from camerafunc.cameraRecorder import get_paths, record_video, get_available_space, get_file_size
+from camerafunc.cameraRecorder import get_paths, record_video, get_available_space, get_file_size, takePicture
 import subprocess
 import threading
 import os
@@ -24,19 +24,20 @@ def display(text:str):
     socketio.emit('update', displayInfo)
 
 # Define the function for each button
-def function_one():
+def start_recording():
     global camera_started
     global cameraOn
-    directory, video_path, _ = get_paths(False)
+    directory, video_paths = get_paths()
+    video_path = video_paths['.h264']
+
     display(f'path: {video_path}')
     cameraOn = True
     display('Recording started')
     recording_thread = threading.Thread(target=record_video, args=(video_path, directory, 3, lambda: cameraOn, lambda: camera_started))    
     recording_thread.start()
-    camera_started = True
 
 
-def function_two():
+def stop_recording():
     global cameraOn
     cameraOn = False
     display('Recording stopped')
@@ -47,8 +48,10 @@ def stop_button():
     os.kill(os.getpid(), signal.SIGINT)
 
 
-def function_three():
-    directory, video_path, video_path_mp4 = get_paths(new=False)
+def save_mp4():
+    directory, video_paths = get_paths(new=False)
+    video_path = video_paths['.h264']
+    video_path_mp4 = video_paths['.mp4']
     if get_available_space(directory) >= get_file_size(video_path)+2:
         subprocess.run([
         "ffmpeg", "-y", "-i", video_path, "-c:v", "copy", video_path_mp4
@@ -58,20 +61,26 @@ def function_three():
         return
     display('mp4 added')
 
+def snap_picture():
+    complete_filename, frame = takePicture(dir='output', filename='testpicture', aspect_ratio=(1920,1080))
+    
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @socketio.on('button_pressed')
 def handle_button_pressed(data):
-    if data == 'button_one':
-        function_one()
-    elif data == 'button_two':
-        function_two()
-    elif data == 'button_three':
-        function_three()
+    if data == 'start_recording':
+        start_recording()
+    elif data == 'stop_recording':
+        stop_recording()
+    elif data == 'save_mp4':
+        save_mp4()
     elif data == 'stop_button':
         stop_button()
+    elif data == 'snap_picture':
+        snap_picture()
 
 
 if __name__ == '__main__':
