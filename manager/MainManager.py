39656b9d61298e73dc12dea29fd6174a664dd07b
GGisMee <gustav.gamstedt@outlook.com>
2025-02-19 09:39:32 +0100

    fixing with web

--- manager/MainManager.py
+++ manager/MainManager.py
@@ -1,6 +1,8 @@
 from detection_with_tracker.detection_with_tracker import DataManager
 from detection_with_tracker.detection_with_tracker import Parameters
 
+from display_functionality.app import RealTimeServer
+
 # python -m manager.MainManager
 
 
@@ -8,11 +10,14 @@ from detection_with_tracker.detection_with_tracker import Parameters
 class MainManager:
     def __init__(self):
         data_manager = DataManager()
-    def setParameters(rpicam:bool=False):
+        server = RealTimeServer()
+        self.parameters = self.setParameters(rpicam=False)
+    def setParameters(self,rpicam:bool=False):
         parameters = Parameters()
         parameters.set_model_paths(hef_path='model/yolov10n.hef', labels_path="detection_with_tracker/coco.txt")
-        if rpicam:
+        if not rpicam:
             parameters.set_input_video(input_video_path=Parameters.DEFAULT_VIDEO_PATHS[1])
         parameters.set_displaying(displayFrame=False,save_frame_debug=True)
         return parameters
-
+    
+    
