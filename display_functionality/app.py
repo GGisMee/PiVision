5137417a2d12d4799a55730b37880cad1258c078
GGisMee <gustav.gamstedt@outlook.com>
2025-02-19 10:50:17 +0100

    work website

--- display_functionality/app.py
+++ display_functionality/app.py
@@ -10,14 +10,17 @@ class RealTimeServer:
         self.socketio = SocketIO(self.app, cors_allowed_origins="*")
         self.current_data = {
             "time": "00:00",
-            "d_front": 150,
+            "d_front": '-',
+            "d_close": '-',
             "num_now": 3,
-            "antal_totalt": 10,
             "extra": ""
         }
         self.running = False  # Track whether updates should run
         self.thread = threading.Thread(target=self._update_loop)
         self.thread.daemon = True
+
+        battery_data = {'level':20}
+        self.socketio.emit('battery_update', battery_data)
 
         @self.app.route("/")
         def index():
@@ -39,14 +42,14 @@ class RealTimeServer:
         """ Return the latest data """
         return self.current_data
 
-    def update_data(self, time_str: str, d_front: float, num_now: int, warning_status: int):
+    def update_data(self, time_str: str, d_front: float,d_close:int, num_now: int, warning_status: int):
         """ Update data and notify clients """
         self.current_data.update({
             "time": time_str,
             "d_front": d_front,
             "num_now": num_now,
             "status": warning_status,
-            "antal_totalt": random.randint(20, 35)
+            "d_close": d_close
         })
         self.socketio.emit("update", self.current_data)
 
@@ -54,8 +57,11 @@ class RealTimeServer:
         """ Background loop to update data periodically """
         while True:
             if self.running:  # Only update when running
-                self.update_data(time.strftime("%H:%M:%S"), random.randint(100, 200), random.randint(1, 10), random.randint(0, 1))
+                self.update_data(time.strftime("%H:%M:%S"), random.randint(100, 200), random.randint(100, 200), random.randint(1, 10), random.randint(0, 1))
             time.sleep(1)
+
+    def set_start(self):
+        """Sets all the functionality to start after button was pressed."""
 
     def run(self, debug=True):
         """ Start the Flask-SocketIO server """
